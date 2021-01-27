abcf80ed0f148c495041077aa886e0e40b0a9d85
ansushina <nastya.sushina@yandex.ru>
2021-01-27 21:12:57 +0300

    DONE

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
-FILE_NAME := ./keyboard_tablet.ko
-MODULES := keyboard_tablet wacom hid_generic usbhid hid
-
 ifneq ($(KERNELRELEASE),)
-	obj-m := keyboard_tablet.o
+	obj-m := tablet_driver.o
 else
 	CURRENT = $(shell uname -r)
 	KDIR = /lib/modules/$(CURRENT)/build
@@ -12,21 +9,10 @@ all:
 	$(MAKE) -C $(KDIR) M=$(PWD) modules
 
 clean:
-	@rm -f *.o .*.cmd .*.flags *.mod.c *.order
-	@rm -f .*.*.cmd *~ *.*~ TODO.*
-	@rm -fR .tmp*
-	@rm -rf .tmp_versions
-
-disclean: clean
-	@rm *.ko *.symvers
-
-remove_modules:
-	sudo rmmod $(MODULES)
-
-insert_module:
-	sudo insmod $(FILE_NAME)
-
-reload_module: all
-	make remove_modules || make insert_module
-
+	rm -rf .tmp_versions
+	rm *.o
+	rm *.mod.c
+	rm *.symvers
+	rm *.order
+	rm .*.cmd
 endif
--- tablet_driver.c
+++ tablet_driver.c
@@ -42,19 +42,33 @@ typedef struct {
 static struct input_dev *keyboard_dev;
 static struct workqueue_struct *wq;
 static int pressed[Y_BUTTONS_COUNT][X_BUTTONS_COUNT];
+static int dop_pressed[2][1];
 
-static int keys[Y_BUTTONS_COUNT][X_BUTTONS_COUNT] = {
+static int buttons_keys[Y_BUTTONS_COUNT][X_BUTTONS_COUNT] = {
     { KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_PRINT, KEY_INSERT},
     { KEY_ESC, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_MINUS, KEY_EQUAL, KEY_BACKSPACE },
     { KEY_TAB, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_LEFTBRACE, KEY_RIGHTBRACE, KEY_BACKSLASH },
-    { KEY_CAPSLOCK, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, KEY_APOSTROPHE, KEY_ENTER },
+    { KEY_CAPSLOCK, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, KEY_APOSTROPHE, KEY_ENTER, KEY_ENTER },
     { KEY_LEFTSHIFT, KEY_LEFTSHIFT, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_DOT, KEY_SLASH, KEY_RIGHTSHIFT, KEY_RIGHTSHIFT },
-    { KEY_LEFTCTRL, KEY_LEFTMETA, KEY_LEFTALT, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_RIGHTALT, 0, 0, KEY_RIGHTCTRL },
+    { KEY_LEFTCTRL, KEY_LEFTMETA, KEY_LEFTALT, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_RIGHTALT, KEY_LEFT, 0, KEY_RIGHT, KEY_RIGHTCTRL },
 };
 
-static int extra_keys[2][3] = {
-    { KEY_DELETE, KEY_UP, KEY_GRAVE },
-    { KEY_LEFT, KEY_DOWN, KEY_RIGHT },
+static char* buttons[Y_BUTTONS_COUNT][X_BUTTONS_COUNT] = {
+    { "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "PRTSC", "INSERT"},
+    { "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BACKSPACE" },
+    { "TAB", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\" },
+    { "CAPSLOCK", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "ENTER", "ENTER" },
+    { "LEFTSHIFT", "LEFTSHIFT", "Z", "X", "C", "V", "B", "N", "M", ",",".","/" , "RIGHTSHIFT", "RIGHTSHIFT" },
+    { "LEFTCTRL", "LEFTMETA", "LEFTALT", "SPACE", "SPACE", "SPACE","SPACE", "SPACE", "SPACE", "RIGHTALT","LEFT", 0, "RIGHT", "RIGHTCTRL" },
+};
+
+static int dop_keys[2][1] = {
+    {KEY_UP},
+    {KEY_DOWN},
+};
+static char * dop_buttons[2][1] = {
+    {"UP"},
+    {"DOWN"},
 };
 
 static void my_wq_function( struct work_struct *work)
@@ -72,7 +86,6 @@ static void my_wq_function( struct work_struct *work)
     tablet = urb->context;
     data = tablet->data;
 
-    // проверяем что урб успешно выполнился
     if (urb->status != 0) {
         printk(KERN_ERR "%s: %s - urb status is %d\n", DRIVER_NAME, __func__, urb->status);
         return;
@@ -110,10 +123,22 @@ static void my_wq_function( struct work_struct *work)
             for (j = 0; j < X_BUTTONS_COUNT; j++)
             {
                 if (pressed[i][j]) {   
-                    input_report_key(keyboard_dev, keys[i][j], 0);
+                    input_report_key(keyboard_dev, buttons_keys[i][j], 0);
                     input_sync(keyboard_dev);
                     pressed[i][j] = 0;
-                    printk(KERN_INFO "%s: pen leaves %x\n", DRIVER_NAME,keys[i][j]);
+                    printk(KERN_INFO "%s: touch ends\n", DRIVER_NAME);
+                }
+            }
+            
+        } 
+        for (i = 0; i < 2; i++) {
+            for (j = 0; j < 1; j++)
+            {
+                if (dop_pressed[i][j]) {   
+                    input_report_key(keyboard_dev, dop_keys[i][j], 0);
+                    input_sync(keyboard_dev);
+                    dop_pressed[i][j] = 0;
+                    printk(KERN_INFO "%s: touch ends\n", DRIVER_NAME);
                 }
             }
             
@@ -123,13 +148,17 @@ static void my_wq_function( struct work_struct *work)
         x = data[4];
         y = data[5]; 
 
-        pressed_key = keys[y / Y_BUTTON_LEN][x / X_BUTTON_LEN];
+        pressed_key = buttons_keys[y / Y_BUTTON_LEN][x / X_BUTTON_LEN];
         pressed[y / Y_BUTTON_LEN][x / X_BUTTON_LEN] = 1;
         if (pressed_key == 0) {
-            pressed_key = extra_keys[y % Y_BUTTON_LEN / (Y_BUTTON_LEN / 2)][x % X_BUTTON_LEN / (X_BUTTON_LEN / 3)];
-        }
-        printk(KERN_INFO "%s: pressed %x\n", DRIVER_NAME, pressed_key);
+            pressed_key = dop_keys[y % Y_BUTTON_LEN / (Y_BUTTON_LEN / 2)][x % X_BUTTON_LEN / (X_BUTTON_LEN)];
+            dop_pressed[y % Y_BUTTON_LEN / (Y_BUTTON_LEN / 2)][x % X_BUTTON_LEN / (X_BUTTON_LEN )] = 1;
+            printk(KERN_INFO "%s: pressed key %s\n", DRIVER_NAME, dop_buttons[y % Y_BUTTON_LEN / (Y_BUTTON_LEN / 2)][x % X_BUTTON_LEN / (X_BUTTON_LEN)]);
+        } else {
+             printk(KERN_INFO "%s: pressed key %s\n", DRIVER_NAME, buttons[y / Y_BUTTON_LEN][x / X_BUTTON_LEN]);
 
+        }
+       
         input_report_key(keyboard_dev, pressed_key, 1);
         input_sync(keyboard_dev);
     }
@@ -152,23 +181,19 @@ static void irq_function(struct urb *urb) {
 }
 
 static int tablet_probe(struct usb_interface *interface, const struct usb_device_id *id) {
-    struct usb_endpoint_descriptor *endpoint; // оконечная точка usb-стройства
-    struct usb_device *usb_device; // usb - устройство
-    tablet_t *tablet; // внутренние данные
+    struct usb_endpoint_descriptor *endpoint; 
+    struct usb_device *usb_device;
+    tablet_t *tablet; 
     int error = -ENOMEM, i, j;
 
-    printk(KERN_INFO "%s: probe checking tablet\n", DRIVER_NAME);
+    printk(KERN_INFO "%s: tablet probe\n", DRIVER_NAME);
 
     endpoint = &interface->cur_altsetting->endpoint[0].desc;
 
-    // проверим, что поключен верный интерфейс
-    // нам нужна только оконечная точка 0x82, так как именно на нее приходят нужные прерывания
     printk(KERN_INFO "%s: endpoint %x\n", DRIVER_NAME, endpoint->bEndpointAddress);
     if (endpoint->bEndpointAddress != 0x82) {
         return -1;
     }
-
-    // СОздание очереди работ
 
     wq = create_workqueue("workqueue");
     if (wq == NULL) {
@@ -176,39 +201,40 @@ static int tablet_probe(struct usb_interface *interface, const struct usb_device
         return -1;
     }
 
-    // Регистрация устройства ввода
-
     keyboard_dev = input_allocate_device();
     if (keyboard_dev == NULL) {
+        flush_workqueue(wq);
+        destroy_workqueue(wq);
         printk(KERN_ERR "%s: allocation device error\n", DRIVER_NAME);
         return -1;
     }
 
-    keyboard_dev->name = "virtual keyboard";
+    keyboard_dev->name = "virtual tablet keyboard";
 
     set_bit(EV_KEY, keyboard_dev->evbit);
     for (i = 0; i < Y_BUTTONS_COUNT; ++i) {
         for (j = 0; j < X_BUTTONS_COUNT; ++j) {
-            if (keys[i][j] != 0) {
-                set_bit(keys[i][j], keyboard_dev->keybit);
+            if (buttons_keys[i][j] != 0) {
+                set_bit(buttons_keys[i][j], keyboard_dev->keybit);
             }
         }
     }
 
     for (i = 0; i < 2; ++i) {
-        for (j = 0; j < 3; ++j) {
-            set_bit(extra_keys[i][j], keyboard_dev->keybit);
+        for (j = 0; j < 1; ++j) {
+            set_bit(dop_keys[i][j], keyboard_dev->keybit);
         }
     }
     
     error = input_register_device(keyboard_dev);
     if (error != 0) {
+        flush_workqueue(wq);
+        destroy_workqueue(wq);
         input_free_device(keyboard_dev);
         printk(KERN_ERR "%s: registration device error\n", DRIVER_NAME);
         return error;
     }
 
-    // Работа с usb-устройством
     usb_device = interface_to_usbdev(interface); 
 
     tablet = kzalloc(sizeof(tablet_t), GFP_KERNEL);
@@ -216,6 +242,8 @@ static int tablet_probe(struct usb_interface *interface, const struct usb_device
 
     tablet->data = (unsigned char *)usb_alloc_coherent(usb_device, USB_PACKET_LEN, GFP_KERNEL, &tablet->data_dma);
     if (!tablet->data) {
+        flush_workqueue(wq);
+        destroy_workqueue(wq);
         input_free_device(keyboard_dev);
         kfree(tablet);
 
@@ -225,6 +253,9 @@ static int tablet_probe(struct usb_interface *interface, const struct usb_device
 
     tablet->irq = usb_alloc_urb( 0, GFP_KERNEL);
     if (!tablet->irq) {
+        flush_workqueue(wq);
+        destroy_workqueue(wq);
+        
         input_free_device(keyboard_dev);
         usb_free_coherent(usb_device, USB_PACKET_LEN, tablet->data, tablet->data_dma);
         kfree(tablet);
@@ -233,19 +264,17 @@ static int tablet_probe(struct usb_interface *interface, const struct usb_device
         return error;
     }
 
-    // заполняем структуру урб
     usb_fill_int_urb(
-        tablet->irq, // ссылка на urb
-        usb_device, // устройство 
-        usb_rcvintpipe(usb_device, endpoint->bEndpointAddress), // адрес оконечной точки
-        tablet->data, // данные пакета
-        USB_PACKET_LEN, // максимальный размер пакета
-        irq_function, // функция обработчика прерывания ->> complete 
-        tablet, // контекст
-        endpoint->bInterval // интервал между прерываниями
+        tablet->irq, 
+        usb_device, 
+        usb_rcvintpipe(usb_device, endpoint->bEndpointAddress), 
+        tablet->data, 
+        USB_PACKET_LEN, 
+        irq_function, 
+        tablet,
+        endpoint->bInterval 
     );
 
-    // отправляем урб на устройство
     usb_submit_urb(tablet->irq, GFP_ATOMIC);
 
     usb_set_intfdata(interface, tablet);
@@ -291,7 +320,6 @@ static struct usb_driver tablet_driver = {
 };
 
 static int __init tablet_driver_init(void) {
-    // регестрируем usb-усройство
     int result = usb_register(&tablet_driver);
     if (result < 0) {
        printk(KERN_ERR "%s: usb register error\n", DRIVER_NAME);
